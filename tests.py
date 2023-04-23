1c3ba5057df02947ba1ca23c71db14f0119bb070
OlenaSakhanda1 <130360301+OlenaSakhanda1@users.noreply.github.com>
2023-04-23 10:23:06 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,34 +1,37 @@
-import unittest
-import xmlrunner
-import io
+from unittest import TestCase
+import pymssql 
 
-out = io.BytesIO()
-unittest.main(
-    testRunner=xmlrunner.XMLTestRunner(output=out),
-    failfast=False,
-    buffer=False,
-    catchbreak=False,
-    exit=False)
+#conn = pymssql.connect(host='Server=LENA\\sqlexpress', trusted=True, database='AdventureWorks2012', as_dict=True)
+conn = pymssql.connect(server="LENA\\sqlexpress",  user="sa", password="sa", database='AdventureWorks2012')
+cursor = conn.cursor()
 
+class TryTesting(TestCase):
+    def test_city_count(self):
+        cursor.execute('SELECT COUNT(DISTINCT [City]) AS city_count FROM [Person].[Address];')
+        value = cursor.fetchone()
+        self.assertTrue(value[0] == 575)
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_max_year(self):
+        cursor.execute('SELECT MAX(YEAR([ModifiedDate])) AS max_year FROM [Person].[Address]')
+        value = cursor.fetchone()
+        self.assertTrue(value[0] <= 2023)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_count_without_file_extension(self):
+        cursor.execute('SELECT COUNT(*) AS count_without_file_extension FROM [Production].[Document] WHERE len(FileExtension) = 0;')
+        value = cursor.fetchone()
+        self.assertTrue(value[0] == 0)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_count_unnormal_level(self):
+        cursor.execute('SELECT COUNT(*) AS count_unnormal_level FROM [Production].[Document] WHERE [DocumentLevel] > 2;')
+        value = cursor.fetchone()
+        self.assertTrue(value[0] == 0)
 
+    def test_count_incorrect_code(self):
+        cursor.execute('SELECT COUNT(*) AS count_incorrect_code FROM [Production].[UnitMeasure] WHERE SUBSTRING ([UnitMeasureCode] ,0 , 1) <> SUBSTRING ([Name],0 , 1);')
+        value = cursor.fetchone()
+        self.assertTrue(value[0] == 0)
 
-if __name__ == '__main__':
-    unittest.main(
-        testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
-        failfast=False,
-        buffer=False,
-        catchbreak=False)
+    def test_rows_count(self):
+        cursor.execute('SELECT COUNT(*) AS rows_count FROM [Production].[UnitMeasure];')
+        value = cursor.fetchone()
+        self.assertTrue(value[0] == 38)
